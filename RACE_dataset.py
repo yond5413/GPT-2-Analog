bbd112da59482f594854de478f48c878eed95bc0
yond5413 <yd2696@columbia.edu>
2024-05-06 17:07:24 -0400

    debugging

--- RACE_dataset.py
+++ RACE_dataset.py
@@ -42,12 +42,15 @@ def preprocess_train(dataset):
     prompt3 = f"Options: A){dataset['article'][0]},B){dataset['article'][1]}, C){dataset['article'][2]}, D){dataset['article'][3]}"
     full_prompt = prompt1 + prompt2 + prompt3
     #print(dataset['answer'])
+    print("dataset length")
+    print(len(dataset))
     label =  [labels[i] for i in dataset['answer']]#[labels[i for i in dataset['answer']]]#choice[dataset['choice']]
     tokenized_dataset = TOKENIZER(full_prompt,padding="max_length", stride=DOC_STRIDE,max_length=MAX_LENGTH,truncation=True)
     tokenized_dataset['label'] = label 
     #-> add tokenizer
     for key, value in tokenized_dataset.items():
         print(f"Length of {key}: {len(value)}")
+    print(len(tokenized_dataset))
     return tokenized_dataset
 
 def preprocess_validation(dataset):
