88a4183f7037753ca3ee981f7c56346c30550b70
yond5413 <yd2696@columbia.edu>
2024-05-06 16:51:51 -0400

    Update RACE_dataset.py

--- RACE_dataset.py
+++ RACE_dataset.py
@@ -38,6 +38,7 @@ def preprocess_train(dataset):
     prompt2 = f"Question: {dataset['question']}\n"
     prompt3 = f"Options: A){dataset['article'][0]},B){dataset['article'][1]}, C){dataset['article'][2]}, D){dataset['article'][3]}"
     full_prompt = prompt1 + prompt2 + prompt3
+    print(dataset['answer'])
     label = labels[dataset['answer']]#choice[dataset['choice']]
     tokenized_dataset = TOKENIZER(full_prompt,padding="max_length", stride=DOC_STRIDE,max_length=MAX_LENGTH,truncation=True)
     tokenized_dataset['label'] = label 
